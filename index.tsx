5e6ecb9c85e56fab7a991c26381e5b6836935849
hao <haohai9309@163.com>
2026-03-16 20:49:08 +0800

    second

--- index.tsx
+++ index.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+// 常规权重
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+
 const container = document.getElementById("root");
 
 if (!container) {
